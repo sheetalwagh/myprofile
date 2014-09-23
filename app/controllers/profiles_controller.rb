ea2f107cfc65e2e1e3c5c9cb153b4b76773446e8
Prelang Builder <builder@prelang.com>
2014-09-23 12:18:07 +0000

    Setting the User of the model on #create to the currently signed in user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -29,6 +29,7 @@ class ProfilesController < ApplicationController
   # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
+    @profile.user = current_user
 
     respond_to do |format|
       if @profile.save
